884b3bb359b3583ebc9fc2bba95cd65877fb7faf
Will Aronow <waronow123@gmail.com>
2022-11-21 14:20:17 -0700

    added missing vuetify img and added mdi icons

--- app.vue
+++ app.vue
@@ -1,7 +1,7 @@
 <template>
   <v-app>
     <v-main>
-      <NuxtWelcome />
+      <VuetifyWelcome />
     </v-main>
   </v-app>
 </template>
--- plugins/vuetify.js
+++ plugins/vuetify.js
@@ -1,4 +1,5 @@
 import { createVuetify } from 'vuetify'
+import '@mdi/font/css/materialdesignicons.css'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
 
@@ -6,6 +7,9 @@ export default defineNuxtPlugin(nuxtApp => {
   const vuetify = createVuetify({
     components,
     directives,
+    icons: {
+      defaultSet: 'mdi'
+    }
   })
   nuxtApp.vueApp.use(vuetify)
 })
